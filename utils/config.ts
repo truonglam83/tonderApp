df3d8016025a91287631ccea622d2f951e205084
zdn-lam-nguyen <lam.nguyen@zodinet.tech>
2023-03-28 21:04:35 +0700

    fix: change api

--- utils/config.ts
+++ utils/config.ts
@@ -1,24 +1,24 @@
 import axios from "axios";
 
-const DOMAIN = "http://54.255.128.135:3002";
+const DOMAIN = "https://tonder.fatcatweb.top";
 
 export const USER = "user";
 export const ACCESS_TOKEN = "accessToken";
 
 export const http = axios.create({
-    baseURL: DOMAIN,
+  baseURL: DOMAIN,
 });
 
 // Cau hinh tat ca request gui di
 http.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
+  (config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
 
-        config.headers["Authorization"] = token ? `Bearer ${token}` : "";
+    config.headers["Authorization"] = token ? `Bearer ${token}` : "";
 
-        return config;
-    },
-    (err) => {
-        return Promise.reject(err);
-    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
 );
